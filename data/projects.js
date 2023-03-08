77825836e0fe698ef16464209292d09654a62fe5
Joe <jcalleya@gmail.com>
2023-03-08 10:17:18 +0000

    adding the angular tign back in now its fxed

--- data/projects.js
+++ data/projects.js
@@ -1,54 +1,41 @@
-export const projectItemsArray = [{
-    name: "No1 South East",
-    description: "Client Project: A Web App featuring Local deals and discounts",
-    techStack: "React",
-    gitHubLink: "https://github.com/nology-tech/no1-south-east",
-    webLink: "https://no1southeast.web.app/",
-    image: "images/no1southeast.png"
-}, {
-    name: "Cocktail Recipe's",
-    description: "Using the Cocktail Recipe API. Search, filter & Save drinks.",
-    techStack: "React - Firebase - Javascript - HTML - CSS - CYPRESS",
-    gitHubLink: "https://github.com/joecalleya/cocktail-api-app",
-    webLink: "https://cocktail-api-app.firebaseapp.com/",
-    image: "images/cocktail-app.png"
-},{
-    name: "Book Finder App",
-    description: "Search Google Books Api and add to favorites list",
-    techStack: "Angular - Firebase - HTML - CSS - JS",
-    gitHubLink: "https://github.com/joecalleya/book-finder-app",
-    webLink: "https://book-finder-app-b2cc3.firebaseapp.com/",
-    image: "images/book-finder-app.png"
-},
-{
-    name: "Random Recipe App",
-    description: "Uses MealDB API, Authenticate users & Save Recipes",
-    techStack: "React - Firebase - Javascript - HTML - CSS",
-    gitHubLink: "https://github.com/joecalleya/recipie-finder-app",
-    webLink: "https://random-recipie.firebaseapp.com/",
-    image: "/images/random-recipie.png"
-}, {
-    name: "Morse Translator",
-    description: "Enter text or Morse to translate between them. Tested using Jest",
-    techStack: "Javascript - CSS - HTML - Jest",
-    gitHubLink: "https://github.com/joecalleya/morse-translator",
-    webLink: "https://joecalleya.github.io/morse-translator/",
-    image: "images/morsecode.png"
-},
-{
-    name: "FIRE Calculator",
-    description: "Financial Independence Retire Early Calculator; how long until you can retire",
-    techStack: "Javascript - HTML - CSS",
-    gitHubLink: "https://github.com/joecalleya/fire-calculator",
-    webLink: "https://joecalleya.github.io/fire-calculator/",
-    image: "images/fire-calculator.png"
-},{
-    name: "HTML Cat Game",
-    description: "Http Cats Meme - Pair Matching Game",
-    techStack: "HTML - CSS - Javascript",
-    gitHubLink: "https://github.com/joecalleya/html-cats-game",
-    webLink: "https://joecalleya.github.io/html-cats-game/",
-    image: "images/catgame.png"
-}
+export const projectItemsArray = [
+
+    {
+        name: "Naturally Naked Soaps",
+        description: "E-commmerce Site for naturally made soap",
+        techStack: "GitLab (CICD) - React - Paypal - Firebase - HTML - CSS - Javascript",
+        gitHubLink: "https://gitlab.com/jcalleya/eCommerceSiteTemplate",
+        webLink: "https://naturally-naked.co.uk/",
+        image: "images/naturally-naked.png"
+    },{
+        name: "Book Finder App",
+        description: "Search Google Books Api and add to favorites list",
+        techStack: "Angular - Firebase - HTML - CSS - JS",
+        gitHubLink: "https://github.com/joecalleya/book-finder-app",
+        webLink: "https://book-finder-app-b2cc3.firebaseapp.com/",
+        image: "images/book-finder-app.png"
+    },{
+        name: "HTML Cat Game",
+        description: "Http Cats Meme - Pair Matching Game",
+        techStack: "HTML - CSS - Javascript",
+        gitHubLink: "https://github.com/joecalleya/html-cats-game",
+        webLink: "https://joecalleya.github.io/html-cats-game/",
+        image: "images/catgame.png"
+    },{
+        name: "Random Recipe App",
+        description: "Uses MealDB API, Authenticate users & Save Recipes",
+        techStack: "React - Firebase - Javascript - HTML - CSS",
+        gitHubLink: "https://github.com/joecalleya/recipie-finder-app",
+        webLink: "https://random-recipie.firebaseapp.com/",
+        image: "/images/random-recipie.png"
+    }, {
+        name: "FIRE Calculator",
+        description: "Financial Independence Retire Early Calculator; how long until you can retire",
+        techStack: "Javascript - HTML - CSS",
+        gitHubLink: "https://github.com/joecalleya/fire-calculator",
+        webLink: "https://joecalleya.github.io/fire-calculator/",
+        image: "images/fire-calculator.png"
+    }
+
 ];
 
